7655fc883791f4c524661c13b1bb7f7bf372b2e8
Eyad Aqrabawi <41790482+eyadaq@users.noreply.github.com>
2025-09-11 09:47:02 +0000

    basic script

---
+++ ex01/PhoneBook/PhoneBook.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 08:40:15 by eaqrabaw          #+#    #+#             */
+/*   Updated: 2025/09/11 08:47:10 by eaqrabaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _PHONEBOOK_H
+# define _PHONEBOOK_H
+
+class PhoneBook{
+	private:
+		int _savedContacts = 0;
+}
+
+#endif
