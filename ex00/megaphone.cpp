cb95f78c3a5abc1714af921b3da7c07566baabad
Eyad Aqrabawi <41790482+eyadaq@users.noreply.github.com>
2025-09-11 08:01:50 +0000

    Doen Ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,17 +6,42 @@
 /*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 07:44:24 by eaqrabaw          #+#    #+#             */
-/*   Updated: 2025/08/31 10:22:12 by eaqrabaw         ###   ########.fr       */
+/*   Updated: 2025/09/11 08:00:03 by eaqrabaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int		main(int argc, char *argv[])
+void 	print_arg(char *s)
 {
-	if (argc > 1 && argv[1])
-		std::cout<<"DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF"<<std::endl;
-	else
+	while (*s)
+	{
+		if (std::isalpha(*s))
+		{
+			std::cout<<(char)std::toupper(*s);
+		}
+		else 
+			std::cout<<(char)*s;
+		s++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 1)
 		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+	else
+	{
+		argv++;
+		while (*argv)
+		{
+			print_arg(*argv);
+			argv++;
+			if (*argv)
+				std::cout<<" ";
+		}
+		std::cout<<std::endl;
+	}
+		
 	return (0);
 }
